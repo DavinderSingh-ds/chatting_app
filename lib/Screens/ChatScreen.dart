c1194d888d1e0e9c175193b318317632c0e344b1
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-03-09 16:21:18 +0530

    Chatting Functionality Added

--- lib/Screens/ChatScreen.dart
+++ lib/Screens/ChatScreen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 import 'package:chatting_app/Helper/FirebaseHelper.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -15,7 +16,11 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   Service service = Service();
+
+  final storeMessage = FirebaseFirestore.instance;
   final auth = FirebaseAuth.instance;
+
+  TextEditingController msg = TextEditingController();
 
   //for current user
   getCurrentUser() {
@@ -51,6 +56,118 @@ class _ChatScreenState extends State<ChatScreen> {
         ],
         title: Text(loginUser!.email.toString()),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text('Messages'),
+          //displaying messages
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: const ShowMessages(),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: Colors.blue, width: 0.5),
+                    ),
+                  ),
+                  child: TextField(
+                    controller: msg,
+                    decoration: const InputDecoration(
+                      hintText: "Enter Message.....",
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    if (msg.text.isNotEmpty) {
+                      storeMessage.collection("Messages").doc().set(
+                        {
+                          "messages": msg.text.trim(),
+                          "user": loginUser!.email.toString(),
+                          "time": DateTime.now()
+                        },
+                      );
+                      msg.clear();
+                    }
+                  },
+                  icon: const Icon(
+                    Icons.send,
+                    color: Colors.teal,
+                  ))
+            ],
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class ShowMessages extends StatelessWidget {
+  const ShowMessages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        //now we can order the message that sent message show in the bottom
+        stream: FirebaseFirestore.instance
+            .collection("Messages")
+            .orderBy("time")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              shrinkWrap: true,
+              primary: true,
+              itemBuilder: (context, i) {
+                QueryDocumentSnapshot x = snapshot.data!.docs[i];
+                return ListTile(
+                  title: Column(
+                    //if user is self then chat showsin end else other user chat shows in start
+                    crossAxisAlignment: loginUser!.email == x['user']
+                        ? CrossAxisAlignment.end
+                        : CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          decoration: BoxDecoration(
+                            color: loginUser!.email == x['user']
+                                ? Colors.blue.withOpacity(0.2)
+                                : Colors.amber.withOpacity(0.4),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(x['messages']),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "User: " + x['user'],
+                                style: const TextStyle(
+                                    fontSize: 10, color: Colors.green),
+                              ),
+                            ],
+                          )),
+                    ],
+                  ),
+                );
+              });
+        });
   }
 }
